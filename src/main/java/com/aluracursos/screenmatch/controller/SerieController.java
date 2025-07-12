dfcf9c4d3e070a416eeb3ef7dfcdf42181694113
Claudio Serrano <claudio.serrano.rojas@gmail.com>
2025-07-12 18:31:45 -0400

    Implemento las funcionalidades top5 y lanzamientos

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -3,24 +3,35 @@ package com.aluracursos.screenmatch.controller;
 import com.aluracursos.screenmatch.dto.SerieDTO;
 
 import com.aluracursos.screenmatch.repository.SerieRepository;
+import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 
 public class SerieController {
-    @Autowired
-    private SerieRepository repository;
+@Autowired
+private SerieService servicio;
 
-    @GetMapping("/series")
-    public List<SerieDTO> obtenerTodasLasSeries(){
-        return repository.findAll().stream()
-                .map(s-> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(),
-                        s.getPoster(),s.getGenero(),s.getActores(),s.getSinopsis()))
-                .collect(Collectors.toList());
+    @GetMapping()
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return servicio.obtenerTodasLasSeries();
     }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obtenerTop5(){
+        return servicio.obtenerTop5();
+    }
+
+    @GetMapping("/lanzamientos")
+    public List<SerieDTO> obtenerLanzamientosMasRecientes(){
+        return servicio.obtenerLanzamientosMasRecientes();
+    }
+
 }
